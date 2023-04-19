ebf76877d4551647f90dee325314a7bc4455f39a
tbrown034 <tbrown034@gmail.com>
2023-04-19 16:58:32 -0500

    some css

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,9 @@
 export default function Index() {
     return (
+        <div className="home">
+            <h1>Trevor's SuperCool React Router Contact App</h1>
+            <h2>Instructions: Click New to add a new contact. Then just enter their name, twitter handle, photo (if you want!) and any other details.</h2>
+            <h3>Enjoy!</h3>
       <p id="zero-state">
         This is a demo for React Router.
         <br />
@@ -9,5 +13,6 @@ export default function Index() {
         </a>
         .
       </p>
+      </div>
     );
   }
